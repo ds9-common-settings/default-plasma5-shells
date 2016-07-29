e3b4f24e063958fcda3164ce6c15625d52606546
Leszek Lesner <leszek.lsner@web.de>
2016-07-29 13:12:08 +0200

    Fix layout.js (by notmart)

--- usr/share/default-settings/plasma5-profile/plasma/shells/org.kde.plasma.desktop/contents/layout.js
+++ usr/share/default-settings/plasma5-profile/plasma/shells/org.kde.plasma.desktop/contents/layout.js
@@ -1,13 +1,12 @@
-
 loadTemplate("org.kde.plasma.desktop.defaultPanel")
-
+   
 var actionPlugins = ConfigFile("plasma-org.kde.plasma.desktop-appletsrc", "ActionPlugins");
-
+   
 var actionPluginsDesktop = ConfigFile(actionPlugins,"0");
 actionPluginsDesktop.writeEntry("MidButton;NoModifier", "org.kde.paste");
 actionPluginsDesktop.writeEntry("RightButton;NoModifier", "org.kde.contextmenu");
 actionPluginsDesktop.writeEntry("wheel:Vertical;NoModifier", "org.kde.switchdesktop");
-
+   
 var rightButton = ConfigFile(actionPluginsDesktop,"RightButton;NoModifier");
 rightButton.writeEntry("_add panel", "true");
 rightButton.writeEntry("_context", "true");
@@ -26,29 +25,27 @@ rightButton.writeEntry("lock widgets", "true");
 rightButton.writeEntry("manage activities", "false");
 rightButton.writeEntry("remove", "true");
 rightButton.writeEntry("run associated application", "false");
-
+   
 delete rightButton;
 delete actionPluginsDesktop;
 delete actionPlugins;
-gc(); 
-
-var id = createActivity("Default Desktop", "org.kde.plasma.folder")
-var desktopsArray = desktopsForActivity(id);
-
+gc();
+   
+var desktopsArray = desktops();
+ 
 for (var j = 0; j < desktopsArray.length; j++) {
-  desktopsArray[j].wallpaperPlugin = 'org.kde.image'
-  desktopsArray[j].wallpaperMode = 'SingleImage'
-  
-  desktopsArray[j].currentConfigGroup = new Array("General");
-  desktopsArray[j].writeConfig("positions","1,7,desktop:/mycomputer.desktop,0,0,desktop:/Network,0,1,desktop:/welcome.desktop,0,2,desktop:/calamares.desktop,0,3")
-  desktopsArray[j].writeConfig("pressToMove",true);
-  desktopsArray[j].writeConfig("showToolbox",false);
-  desktopsArray[j].writeConfig("popups",false);
-  desktopsArray[j].writeConfig("selectionMarkers",false);
-  desktopsArray[j].writeConfig("sortMode","-1");
-  //         [Containments][18][Wallpaper][org.kde.image][General]
-  desktopsArray[j].currentConfigGroup = new Array("Wallpaper", "org.kde.image", "General");
-  desktopsArray[j].writeConfig("Image", "file:///usr/share/default-settings/default-wallpaper.png");
-  desktopsArray[j].writeConfig("FillMode","2");
+    desktopsArray[j].wallpaperPlugin = 'org.kde.image'
+    desktopsArray[j].wallpaperMode = 'SingleImage'
+ 
+    desktopsArray[j].currentConfigGroup = new Array("General");
+    desktopsArray[j].writeConfig("positions","1,7,desktop:/mycomputer.desktop,0,0,desktop:/Network,0,1,desktop:/welcome.desktop,0,2,desktop:/calamares.desktop,0,3")
+    desktopsArray[j].writeConfig("pressToMove",true);
+    desktopsArray[j].writeConfig("showToolbox",false);
+    desktopsArray[j].writeConfig("popups",false);
+    desktopsArray[j].writeConfig("selectionMarkers",false);
+    desktopsArray[j].writeConfig("sortMode","-1");
+//         [Containments][18][Wallpaper][org.kde.image][General]
+    desktopsArray[j].currentConfigGroup = new Array("Wallpaper", "org.kde.image", "General");
+    desktopsArray[j].writeConfig("Image", "file:///usr/share/default-settings/default-wallpaper.png");
+    desktopsArray[j].writeConfig("FillMode","2");
 }
-
